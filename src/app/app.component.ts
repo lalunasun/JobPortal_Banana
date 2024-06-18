dc4778631518c570457fc981b967b6ee9751c2b9
Luna <laluna.sun@gmail.com>
2024-06-18 14:08:47 -0400

    add home search box

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'jobPortalApp';
+  //title = 'jobPortalApp';
+  isLoggedIn:boolean=false;
+  userInfo:any;
+
+  constructor(){
+    const UserData = localStorage.getItem('jobLoginUser');
+    if(UserData == null){
+      this.isLoggedIn = false;
+    }else{
+      this.isLoggedIn = true;
+      this.userInfo = JSON.parse(UserData);
+    }
+  }
+  logoff(){
+    localStorage.removeItem('jobLoginUser');
+    this.isLoggedIn = false;
+  }
 }
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { JobService } from 'src/app/service/job.service';
+import {Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -14,11 +15,13 @@ export class LoginComponent implements OnInit {
     "Password": ""
   }
   
-  constructor(private jobSrv:JobService) { }
+  constructor(private jobSrv:JobService,private router:Router) { }
 
   onLogin(){
     this.jobSrv.login(this.loginObj).subscribe((res:any)=>{
       if (res.result){
+        localStorage.setItem('jobLoginUser',JSON.stringify(res.data));
+        this.router.navigateByUrl('/home');
         alert('User Logged in Success')
       } else{
         alert(res.message)
